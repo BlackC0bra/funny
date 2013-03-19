8a561fabc6ea09dce2f213e7a5ebd3f7eb87b445
BlackC0bra <ext3cobra@yandex.ru>
2013-03-19 19:50:34 +0600

    test 1

---
+++ funny.rb
@@ -0,0 +1,12 @@
+require 'open-uri'
+url = 'http://www.anekdot.ru/scripts/rand_anekdot.php'
+page = open(url)
+text = page.read
+joke = text.scan(%r{<div class="text" id="txt_id_-{0,}\d{1,}">(.*?)</div>})
+n = joke.length
+str = joke[rand(n)]
+str = str[0].gsub(/<br \/>/){"\n"}
+str = str.gsub(/&quot;/){"\""}
+puts str
+
+#puts joke.gsub(/<br \/>/){"\n"}
